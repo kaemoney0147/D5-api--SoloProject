2bfa88a9051875b3107c1ca047093dcaf6b42193
abiodun <114649936+kaemoney0147@users.noreply.github.com>
2023-01-11 13:08:56 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,32 +13,26 @@ import {
 import cors from "cors";
 
 const server = express();
-const publicFolderPath = join(process.cwd(), "./public");
-server.use(express.static(publicFolderPath));
-
 const urlList = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
 
-server.use(
-  cors({
-    origin: (origin, corsNext) => {
-      // If you want to connect FE to this BE you must use cors middleware
-      console.log("ORIGIN: ", origin);
+const corsOpts = {
+  origin: (origin, corsNext) => {
+    console.log("CURRENT ORIGIN: ", origin);
+    if (!origin || urlList.indexOf(origin) !== -1) {
+      // If current origin is in the whitelist you can move on
+      corsNext(null, true);
+    } else {
+      // If it is not --> error
+      corsNext(
+        createHttpError(400, `Origin ${origin} is not in the whitelist!`)
+      );
+    }
+  },
+};
 
-      if (!origin || urlList.indexOf(origin) !== -1) {
-        // if origin is in the urlList we can move next
-        corsNext(null, true);
-      } else {
-        // if origin is NOT in the urlList --> trigger an error
-        corsNext(
-          createHttpError(
-            400,
-            `Cors Error! Your origin ${origin} is not in the list!`
-          )
-        );
-      }
-    },
-  })
-);
+const publicFolderPath = join(process.cwd(), "./public");
+server.use(express.static(publicFolderPath));
+server.use(cors(corsOpts));
 
 const port = process.env.PORT;
 server.use(express.json());
